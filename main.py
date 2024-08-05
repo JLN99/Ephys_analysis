a86b38adf7ffe044dd5d175b36ad5029d0769c6f
JLN99 <julian.nausester@gmail.com>
2024-08-05 22:59:22 +0200

    implementation of data frame generation from ascii files

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
+'''
+Starting Steps: 
+    1. Enter/Create the virtuall enviroment: comand: pipenv shell
+    -> opens or generates virt. envirment depending on the existing pipfile. - allways the same package versions
+    2. lunching python files via the shell terminal: python filename.py 
+    3. after editing or new code push to github!
+'''
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 import functions as func
 
-path_file = 'files\\20240620_a1b2g2-EtOH-1h_zelle1.asc' # load file Path
+path_file = 'ascii-files\\20240620_a1b2g2-EtOH-1h_zelle2.asc' # load file Path
 
 
 # Pfad zur .asc-Datei
-data_frames = func.read_multiple_measurements(path_file)
+df = func. parse_measurements(path_file)
 
-# Spaltennamen in jedem DataFrame bereinigen
-for df in data_frames:
-    df.columns = df.columns.str.strip()
-
-# Überprüfen der Daten
-for i, df in enumerate(data_frames):
-    print(f"DataFrame {i + 1}:")
-    print(df.head())
-    print(df.columns)
+print(df.head())
+print(df.columns)
 
 
 #     # DataFrame in eine CSV-Datei speichern
@@ -27,14 +29,13 @@ for i, df in enumerate(data_frames):
 # # CSV-Datei öffnen (kann in einem Texteditor oder Tabellenkalkulationsprogramm geöffnet werden)
 # import os
 # os.startfile('dataframe_output.csv')
-
 # Graphen erstellen
-df['"Time[s]"'] = pd.to_numeric(df['"Time[s]"'], errors='coerce')
-df['"I-mon[A]"'] = pd.to_numeric(df['"I-mon[A]"'], errors='coerce')
+df['Time[s]'] = pd.to_numeric(df['Time[s]'], errors='coerce')
+#df['I-mon[A]"'] = pd.to_numeric(df['"I-mon[A]"'], errors='coerce')
 
 # Graph erstellen
 plt.figure(figsize=(10, 6))
-plt.plot(df['"Time[s]"'], df['"I-mon[A]"'], marker='x', linestyle='-')
+plt.plot(df['Time[s]'], df['Series_2_6'], marker='x', linestyle='-')
 plt.xlabel('Time [s]')
 plt.ylabel('I-mon [A]')
 plt.title('Messung')
